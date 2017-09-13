6d36f9904074c5773679065a38f7b9c02f70a092
Forest Gregg <fgregg@uchicago.edu>
2017-09-13 12:00:13 -0500

    package structure

---
+++ setup.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+try:
+    from setuptools import setup
+except ImportError :
+    raise ImportError("setuptools module required, please go to https://pypi.python.org/pypi/setuptools and follow the instructions for installing setuptools")
+
+setup(
+    name='pysettrie',
+    url='https://github.com/datamade/census_area',
+    #version='0.2',
+    author='Márton Miháltz ',
+    description='Efficient storage and querying of sets of sets using the trie data structure',
+    packages=['settrie'],
+    install_requires=['sortedcontainers'],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Operating System :: MacOS :: MacOS X',
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: POSIX',
+        'Programming Language :: Python :: 3.5',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Scientific/Engineering',
+        'Topic :: Scientific/Engineering :: Information Analysis'],
+)
